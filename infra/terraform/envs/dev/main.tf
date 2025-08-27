0a1a494f25d7ddd24e6b29cc4a771e8999c29ba7
Anner <anr.aquino@gmail.com>
2025-08-26 19:10:38 -0500

    updated main.tf

--- infra/terraform/envs/dev/main.tf
+++ infra/terraform/envs/dev/main.tf
@@ -25,9 +25,17 @@ resource "aws_subnet" "public_b" {
 }
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
-  route { cidr_block = "0.0.0.0/0" gateway_id = aws_internet_gateway.igw.id }
-  tags = { Name = "${var.service_name}-rtb-public" }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.service_name}-rtb-public"
+  }
 }
+
 resource "aws_route_table_association" "a" { subnet_id = aws_subnet.public_a.id route_table_id = aws_route_table.public.id }
 resource "aws_route_table_association" "b" { subnet_id = aws_subnet.public_b.id route_table_id = aws_route_table.public.id }
 
