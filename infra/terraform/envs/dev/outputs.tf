83659e79d253c4254f2a7707ad54b425030776b3
Anner <anr.aquino@gmail.com>
2025-08-26 22:12:22 -0500

    Updated outputs

--- infra/terraform/envs/dev/outputs.tf
+++ infra/terraform/envs/dev/outputs.tf
@@ -1,2 +1,16 @@
-output "alb_dns_name" { value = aws_lb.app.dns_name }
-output "ecr_repo_url" { value = aws_ecr_repository.repo.repository_url }
+# Safe: uses the local computed with try(one(resource[*]), one(data[*]))
+output "ecr_repo_url" {
+  value = local.ecr_repo_url
+}
+
+output "alb_dns_name" {
+  value = aws_lb.app.dns_name
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.this.name
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.app.name
+}
