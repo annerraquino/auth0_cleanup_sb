5716d017a66cbc70bb63bbc1d07c097397859478
Anner <anr.aquino@gmail.com>
2025-08-29 17:20:14 -0500

    Updated outputs

--- infra/terraform/envs/dev/outputs.tf
+++ infra/terraform/envs/dev/outputs.tf
@@ -1,10 +1,6 @@
-# Safe: uses the local computed with try(one(resource[*]), one(data[*]))
-output "ecr_repo_url" {
-  value = local.ecr_repo_url
-}
-
 output "alb_dns_name" {
-  value = aws_lb.app.dns_name
+  description = "Public URL of the Application Load Balancer"
+  value       = aws_lb.app.dns_name
 }
 
 output "ecs_cluster_name" {
@@ -13,4 +9,9 @@ output "ecs_cluster_name" {
 
 output "ecs_service_name" {
   value = aws_ecs_service.app.name
+}
+
+# Helpful to see what image was deployed in this run
+output "image_uri" {
+  value = var.image_uri
 }
